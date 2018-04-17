ab9f575dca8c8d2d2568ae4fe013d9eaeb5efb19
Tirumala R Marri <mtreddy@gmail.com>
2018-04-17 09:15:15 -0700

    ore hep info

--- algo/dyn/down_to_zero.cpp
+++ algo/dyn/down_to_zero.cpp
@@ -1,6 +1,48 @@
 /*
+ou are given  queries. Each query consists of a single number . You can perform any of the  operations on  in each move:
+
+1: If we take 2 integers  and  where , , then we can change 
+
+2: Decrease the value of  by .
+
+Determine the minimum number of moves required to reduce the value of  to .
+
+Input Format
+
+The first line contains the integer . 
+The next  lines each contain an integer, .
+
+Constraints
+
+ 
+
+Output Format
+
+Output  lines. Each line containing the minimum number of moves required to reduce the value of  to .
+
+Sample Input
+
+2
+3
+4
+Sample Output
+3
+3
+Explanation
+For test case 1, We only have one option that gives the minimum number of moves. 
+Follow  ->  ->  -> . Hence,  moves.
+
+For the case 2, we can either go  ->  ->  ->  ->  or  ->  ->  -> . The 2nd option is more optimal. Hence,  moves.
+ *
+ */
+/*
  * Complete the downToZero function below.
  */
+#include<iostream>
+#include<fstream>
+#include<map>
+#include<math.h>
+using namespace std;
 map<int, int> minA;
 int minVal(int x){
     int minV = INT_MAX;
@@ -25,6 +67,8 @@ int downToZero(int n) {
     /*
      * Write your code here.
      */
+    int res = minVal(n);
+        cout << "result is"<< res << endl;
    return minVal(n);
 }
 
@@ -39,10 +83,9 @@ int main()
     for (int q_itr = 0; q_itr < q; q_itr++) {
         int n;
         cin >> n;
+        cout << n << " is n " << q << " is q" << endl;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
         int result = downToZero(n);
-
         fout << result << "\n";
     }
 
