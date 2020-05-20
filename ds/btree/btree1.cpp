030267571c59045ad5709e74e93f10324d991213
Tirumala Marri <tirmarri@amd.com>
2020-05-20 16:09:39 -0500

    Using queues to insert nodes

--- ds/btree/btree1.cpp
+++ ds/btree/btree1.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
-
+#include<queue>
+#include<bits/stdc++.h> 
 using namespace std;
 
 
-typedef struct node
+
+struct node
 {
    int data;
    node * left;
    node * right;
-}node;
+   node(int key){
+	   data =key;
+	   left = right = NULL;
+   }
+};
 
 void print_tree(node *root){
     if(root != NULL) {
@@ -17,12 +23,45 @@ void print_tree(node *root){
         print_tree(root->right);
     }
 }
+
+void inorder_print(node *node){
+	if(node == NULL)
+		return;
+	if(node->left != NULL)
+		inorder_print(node->left);
+	cout << " " << node->data;
+	if(node->left != NULL)
+		inorder_print(node->right);
+}
+void insert_q(node *tt, int data)
+{
+	queue<node*> q;
+	q.push(tt);
+
+	while(!q.empty()){
+		node *tmp = q.front();
+		q.pop();
+
+		if(tmp->left != NULL) q.push(tmp->left);
+		else {
+			tmp->left = new node(data);
+			break;
+		}
+		if(tmp->right != NULL) q.push(tmp->right);
+		else {
+			tmp->right = new node(data);
+			break;
+		}
+
+
+	}
+}
 node * insert(node * root, int value) {
 
     node *x = root;
     node *y = NULL;
     node *z = NULL;
-    z = new node;
+    z = new node(value);
     z->data=value;
     x = root;
     y = x;
@@ -40,10 +79,22 @@ node * insert(node * root, int value) {
 
 int main() {
     node *root = NULL;
+#if 0
 	root = insert(root, 4);
 	root = insert(root, 3);
 	root = insert(root, 2);
 	root = insert(root, 1);
 	root = insert(root, 7);
     print_tree(root);
+#else
+	node *tmp;
+	root = new node(1);
+	insert_q(root, 2);
+	insert_q(root, 3);
+	insert_q(root, 4);
+	insert_q(root, 5);
+	insert_q(root, 2);
+
+	inorder_print(root);
+#endif
 }
